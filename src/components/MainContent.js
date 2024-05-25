a782970bef9d30a1b3bb5180b7310b9eb57978d2
M2 <m2_olive@M2MacBook-Air.elecom>
2024-05-26 02:42:53 +0900

    Add Footer, Header, and MainContent components

---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <p>Welcome to my website. Here is the main content.</p>
+    </main>
+  );
+}
+
+export default MainContent;
