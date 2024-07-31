177cad449941f0182009d4e60045c802af713465
M2 <m2_olive@M2MacBook-Air.local>
2024-07-31 16:14:04 +0900

    add header click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 // src/components/Header.js
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css'; // 必要に応じてCSSをインポート
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const handleTitleClick = (e) => {
+    if (location.pathname === '/') {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
   return (
     <header className="header">
-      <h1 className="title">Olive</h1>
+      <Link to="/" className="titleLink" onClick={handleTitleClick}>
+        <h1 className="title">Olive</h1>
+      </Link>
       <div className="menuIcon" onClick={toggleMenu}>
         &#9776;
       </div>
